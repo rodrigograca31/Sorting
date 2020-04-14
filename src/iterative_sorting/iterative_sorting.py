115680da99e233e13c89b709ed951b967ea7e3e0
Rodrigo Graça <rodrigograca31@gmail.com>
2020-04-14 15:26:33 +0100

    perf: ⚡️ impoved sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,14 +1,36 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
+    for i in range(0, len(arr)):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc)
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+
+        # TO-DO: swap
+        if cur_index != smallest_index:
+            (arr[cur_index], arr[smallest_index]) = (
+                arr[smallest_index],
+                arr[cur_index],
+            )
+            pass
+
+    return arr
+
+
+def selection_sort2(arr):
+    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        for curr in range(i, len(arr)):
-            if arr[curr] < arr[smallest_index]:
-                smallest_index = curr
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         if cur_index != smallest_index:
@@ -42,8 +64,20 @@ def count_sort(arr, maximum=-1):
     return arr
 
 
-data = [7, 3, 8, 23, 7, 9, 4, 2, 8, 0, 34, 2, 7, 9, 3, 1, 9, 6, 8, -6, -9, -10]
+data = [1, 3, 2]
 
-print(len(data))
-# print(selection_sort(data))
-print(bubble_sort(data))
+
+import time
+
+times = 10000000
+
+
+start = time.time()
+for x in range(times):
+    (selection_sort(data))
+print("Imp 1: \t%.8f" % float(time.time() - start))
+
+start = time.time()
+for x in range(times):
+    (selection_sort2(data))
+print("Imp 2: \t%.8f" % float(time.time() - start))
